a65a39f0d30b2ceeea6d990c9eecddbe69fe3d21
Alice <alice.i.cecile@gmail.com>
2021-12-24 18:21:39 -0500

    Add released and just_released functionality
    Closes #2.

--- src/lib.rs
+++ src/lib.rs
@@ -15,9 +15,12 @@ use strum::IntoEnumIterator;
 /// - a central [ActionState] resource, which stores the current input state in an source-agnostic fashion
 ///
 /// Systems:
-/// - [reset_action_state], which resets the action state each frame
-/// - [update_action_state] and [update_action_state_gamepads], which collects the input from the corresponding input type to update the action state
-///     - labeled [InputMapSystem]
+/// - [tick_action_state], which resets the pressed and just_pressed fields of the [ActionState] each frame
+///     - labeled [InputMapSystem::Reset]
+/// - [update_action_state] and [update_action_state_gamepads], which collects the [Input] from the corresponding input type to update the [ActionState]
+///     - labeled [InputMapSystem::Read]
+/// - [release_action_state], which releases all actions which are not currently pressed by any system
+///     - labeled [InputMapSystem::Release]
 pub struct InputManagerPlugin<InputAction: InputActionEnum> {
     _phantom: PhantomData<InputAction>,
 }
@@ -40,7 +43,11 @@ pub trait InputActionEnum:
 }
 
 #[derive(SystemLabel, Clone, Hash, Debug, PartialEq, Eq)]
-pub struct InputMapSystem;
+pub enum InputMapSystem {
+    Reset,
+    Read,
+    Release,
+}
 
 impl<InputAction: InputActionEnum> Plugin for InputManagerPlugin<InputAction> {
     fn build(&self, app: &mut App) {
@@ -48,14 +55,24 @@ impl<InputAction: InputActionEnum> Plugin for InputManagerPlugin<InputAction> {
             .init_resource::<InputMap<InputAction, MouseButton>>()
             .init_resource::<InputMap<InputAction, GamepadButton, GamepadButtonType>>()
             .init_resource::<ActionState<InputAction>>()
+            .add_system(
+                tick_action_state::<InputAction>
+                    .label(InputMapSystem::Reset)
+                    .before(InputMapSystem::Read),
+            )
             .add_system_set_to_stage(
                 CoreStage::PreUpdate,
                 SystemSet::new()
                     .with_system(update_action_state::<InputAction, KeyCode>)
                     .with_system(update_action_state::<InputAction, MouseButton>)
                     .with_system(update_action_state_gamepads::<InputAction>)
-                    .label(InputMapSystem)
+                    .label(InputMapSystem::Read)
                     .after(InputSystem),
+            )
+            .add_system(
+                release_action_state::<InputAction>
+                    .label(InputMapSystem::Release)
+                    .after(InputMapSystem::Read),
             );
     }
 }
@@ -198,41 +215,107 @@ impl<InputAction: InputActionEnum> InputMap<InputAction, GamepadButton, GamepadB
 #[derive(Component)]
 pub struct ActionState<InputAction: InputActionEnum> {
     pressed: HashMap<InputAction, bool>,
+    pressed_this_tick: HashMap<InputAction, bool>,
     just_pressed: HashMap<InputAction, bool>,
+    just_released: HashMap<InputAction, bool>,
 }
 
 impl<InputAction: InputActionEnum> ActionState<InputAction> {
+    /// Clears all just_pressed and just_released state
+    ///
+    /// Also clears the internal field `pressed_this_tick`, so inputs can be correctly released
+    pub fn tick(&mut self) {
+        self.just_pressed = Self::default_map();
+        self.just_released = Self::default_map();
+        self.pressed_this_tick = Self::default_map();
+    }
+
+    /// Press the `action`
+    pub fn press(&mut self, action: InputAction) {
+        self.pressed.insert(action, true);
+        self.pressed_this_tick.insert(action, true);
+        if !self.pressed(action) {
+            self.just_pressed.insert(action, true);
+        }
+    }
+
+    /// Release the `action`
+    pub fn release(&mut self, action: InputAction) {
+        self.pressed.insert(action, false);
+
+        if self.pressed(action) {
+            self.just_released.insert(action, true);
+        }
+    }
+
+    /// Releases all actions
+    pub fn release_all(&mut self) {
+        for action in InputAction::iter() {
+            self.release(action);
+        }
+    }
+
+    /// Is an action currently pressed?
     pub fn pressed(&self, action: InputAction) -> bool {
         *self.pressed.get(&action).unwrap()
     }
 
+    /// Was this action pressed since the last time [tick](ActionState::tick) was called?
     pub fn just_pressed(&self, action: InputAction) -> bool {
         *self.just_pressed.get(&action).unwrap()
+    }
+
+    /// Is an action currently released?
+    ///
+    /// This is always the logical negation of [pressed](ActionState::pressed)
+    pub fn released(&self, action: InputAction) -> bool {
+        !*self.pressed.get(&action).unwrap()
+    }
+
+    /// Was this action pressed since the last time [tick](ActionState::tick) was called?
+    pub fn just_released(&self, action: InputAction) -> bool {
+        *self.just_pressed.get(&action).unwrap()
+    }
+
+    /// Release all actions that were not pressed this tick
+    pub fn release_unpressed(&mut self) {
+        for action in InputAction::iter() {
+            if !*self.pressed_this_tick.get(&action).unwrap() {
+                self.release(action);
+            }
+        }
+    }
+
+    /// Creates a Hashmap with all of the possible InputAction variants as keys, and false as the values
+    fn default_map() -> HashMap<InputAction, bool> {
+        // PERF: optimize construction through pre-allocation or constification
+        let mut map = HashMap::default();
+
+        for action in InputAction::iter() {
+            map.insert(action, false);
+        }
+        map
     }
 }
 
 impl<InputAction: InputActionEnum> Default for ActionState<InputAction> {
     fn default() -> Self {
-        let mut pressed = HashMap::<InputAction, bool>::default();
-        let mut just_pressed = HashMap::<InputAction, bool>::default();
-
-        for action in InputAction::iter() {
-            pressed.insert(action, false);
-            just_pressed.insert(action, false);
-        }
-
         Self {
-            pressed,
-            just_pressed,
+            pressed: Self::default_map(),
+            pressed_this_tick: Self::default_map(),
+            just_pressed: Self::default_map(),
+            just_released: Self::default_map(),
         }
     }
 }
 
-/// Resets the action state to "nothing is pressed" each frame
-pub fn reset_action_state<InputAction: InputActionEnum>(
+/// Clears the just-pressed and just-released values of the [ActionState]
+///
+/// Also resets the internal `pressed_this_tick` field, used to track whether or not to release an action
+pub fn tick_action_state<InputAction: InputActionEnum>(
     mut action_state: ResMut<ActionState<InputAction>>,
 ) {
-    *action_state = ActionState::default();
+    action_state.tick();
 }
 
 /// Fetches an [Input] resource to update [ActionState] according to the [InputMap]
@@ -247,11 +330,7 @@ pub fn update_action_state<
     for action in InputAction::iter() {
         // A particular input type can add to the action state, but cannot revert it
         if input_map.pressed(action, &*input) {
-            action_state.pressed.insert(action, true);
-        }
-
-        if input_map.just_pressed(action, &*input) {
-            action_state.just_pressed.insert(action, true);
+            action_state.press(action);
         }
     }
 }
@@ -272,10 +351,13 @@ pub fn update_action_state_gamepads<InputAction: InputActionEnum>(
             if gamepad_map.pressed(action, &*gamepad_input, gamepad) {
                 action_state.pressed.insert(action, true);
             }
-
-            if gamepad_map.just_pressed(action, &*gamepad_input, gamepad) {
-                action_state.just_pressed.insert(action, true);
-            }
         }
     }
+}
+
+/// Releases all [ActionState] actions that were not pressed since the last time [tick_action_state] ran
+pub fn release_action_state<InputAction: InputActionEnum>(
+    mut action_state: ResMut<ActionState<InputAction>>,
+) {
+    action_state.release_unpressed();
 }
