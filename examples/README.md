861c48630d4681ab60b12d17fa8579934ebd2e92
Alice Cecile <alice.i.cecile@gmail.com>
2022-01-23 14:17:05 -0500

    Add `minimal` example

--- examples/README.md
+++ examples/README.md
@@ -2,7 +2,7 @@
 
 You can run examples using `cargo run --example EXAMPLE_NAME`.
 
-Start with the `single_player` example for basic usage.
+Start with the `minimal` example to get a basic sense of the plugin!
 
 Note that each subfolder stores a seperate, more complete example.
 To run it, set `EXAMPLE_NAME` to the name of that folder.
---
+++ examples/minimal.rs
@@ -0,0 +1,49 @@
+use bevy::prelude::*;
+use leafwing_input_manager::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(MinimalPlugins)
+        // This plugin maps inputs to an input-type agnostic action-state
+        // We need to provide it with an enum which stores the possible actions a player could take
+        .add_plugin(InputManagerPlugin::<Action>::default())
+        // The InputMap and ActionState components will be added to any entity with the Player component
+        .add_startup_system(spawn_player)
+        // Read the ActionState in your systems using queries!
+        .add_system(jump)
+        .run();
+}
+
+#[derive(Actionlike, PartialEq, Eq, Clone, Copy, Hash, Debug, EnumIter)]
+enum Action {
+    Run,
+    Jump,
+}
+
+#[derive(Component)]
+struct Player;
+
+fn spawn_player(mut commands: Commands) {
+    // Adding new bindings is easy!
+    let mut input_map = InputMap::default();
+    input_map.insert(Action::Jump, KeyCode::Space);
+
+    commands
+        .spawn()
+        .insert(Player)
+        .insert_bundle(InputManagerBundle::<Action> {
+            // Stores "which actions are being used"
+            action_state: ActionState::default(),
+            // Stores input bindings
+            input_map,
+        });
+}
+
+// Query for the ActionState component in your game logic systems!
+fn jump(query: Query<&ActionState<Action>, With<Player>>) {
+    let action_state = query.single();
+    // Each action variant has a virtual button of its own
+    if action_state.just_pressed(Action::Jump) {
+        println!("I'm jumping!");
+    }
+}
