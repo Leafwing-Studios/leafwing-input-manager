fe6c6275ac8c35a261458c7a6207293d88add6a6
Alice Cecile <alice.i.cecile@gmail.com>
2022-01-22 21:49:28 -0500

    Basic menu structure for input binding example

--- examples/game/gameplay.rs
+++ examples/game/gameplay.rs
@@ -1,8 +1,9 @@
 use bevy::prelude::*;
+use derive_more::Display;
 use leafwing_input_manager::prelude::*;
 use strum_macros::EnumIter;
 
-#[derive(Actionlike, PartialEq, Eq, Clone, Copy, Hash, Debug, EnumIter)]
+#[derive(Actionlike, PartialEq, Eq, Clone, Copy, Hash, Debug, EnumIter, Display)]
 pub enum GameAction {
     Jump,
     MoveLeft,
--- examples/game/input_binding_menu.rs
+++ examples/game/input_binding_menu.rs
@@ -1,5 +1,5 @@
 use bevy::prelude::*;
-use leafwing_input_manager::input_map::{InputMap, UserInput};
+use leafwing_input_manager::{input_map::InputMap, user_input::UserInput};
 use strum::IntoEnumIterator;
 
 use crate::common_ui::{FontType, Fonts, UiSetupLabels};
@@ -24,6 +24,19 @@ fn spawn_input_menu(mut commands: Commands, fonts: Res<Fonts>) {
         font_size: 30.0,
         color: Color::BLACK,
     };
+
+    let column_heading_style = TextStyle {
+        font: fonts.get_handle(FontType::Body),
+        font_size: 20.0,
+        color: Color::BLACK,
+    };
+
+    let body_style = TextStyle {
+        font: fonts.get_handle(FontType::Body),
+        font_size: 16.0,
+        color: Color::BLACK,
+    };
+
     let title_alignment = TextAlignment::default();
 
     // Global node for all other UI to live in
@@ -40,7 +53,7 @@ fn spawn_input_menu(mut commands: Commands, fonts: Res<Fonts>) {
         })
         .id();
 
-    // Stores our title, column headings and input selectors
+    // Stores our title, column headings, input selectors and quit menu button
     let menu_node = commands
         .spawn_bundle(NodeBundle {
             style: Style {
@@ -79,41 +92,34 @@ fn spawn_input_menu(mut commands: Commands, fonts: Res<Fonts>) {
 
     commands.entity(title_node).add_child(title_text);
 
-    // Contains the body of the menu
-    // This stores a number of distinct input binding rows,
-    // stacked vertically on top of each other (from top to bottom)
-    let menu_body_node = commands
-        .spawn_bundle(NodeBundle {
-            style: Style {
-                size: Size::new(Val::Percent(100.0), Val::Percent(60.0)),
-                justify_content: JustifyContent::Center,
-                flex_direction: FlexDirection::ColumnReverse,
-                ..Default::default()
-            },
-            color: Color::PURPLE.into(),
-            ..Default::default()
-        })
-        .id();
+    // This call spawns an entity within the function as a side effect
+    // returning the Entity reference of the row itself
+    let column_headings_row = spawn_input_binding_row(
+        &mut commands,
+        "Actions".to_string(),
+        "Primary Input".to_string(),
+        "Secondary Input".to_string(),
+        column_heading_style,
+    );
 
     commands
         .entity(menu_node)
-        .push_children(&[title_node, menu_body_node]);
+        .push_children(&[title_node, column_headings_row]);
 
-    // Begin with the default input map settings
-    let input_map = InputMap::<GameAction>::default();
+    // We need a variable number of distinct input binding rows,
+    // stacked vertically on top of each other (from top to bottom)
 
     for action in GameAction::iter() {
-        // Fetch the initial bindings from the input map
-        // In this game, our inputs are not split by input modes, so we pass in `input_mode = None`
-        let input_bindings = input_map.get(action, None);
         // In this example, we only permit two input map bindings per action
-        let input_1 = input_bindings.get_at(0);
-        let input_2 = input_bindings.get_at(1);
-
-        // This call spawns an entity within the function as a side effect
-        // returning the Entity reference of the row itself
-        let input_binding_row_entity =
-            spawn_input_binding_row(&mut commands, action, input_1, input_2);
+        let input_binding_row_entity = spawn_input_binding_row(
+            &mut commands,
+            action.to_string(),
+            // The strings displayed for our input bindings start empty,
+            // but are immediately set when the input map is initialized
+            "".to_string(),
+            "".to_string(),
+            body_style.clone(),
+        );
 
         commands
             .entity(menu_node)
@@ -123,15 +129,23 @@ fn spawn_input_menu(mut commands: Commands, fonts: Res<Fonts>) {
 
 fn spawn_input_binding_row(
     commands: &mut Commands,
-    action: GameAction,
-    input_1: Option<&UserInput>,
-    input_2: Option<&UserInput>,
+    action: String,
+    input_1: String,
+    input_2: String,
+    text_style: TextStyle,
 ) -> Entity {
     let row_node = commands
         .spawn_bundle(NodeBundle {
             style: Style {
                 size: Size::new(Val::Percent(100.0), Val::Percent(10.0)),
+                padding: Rect {
+                    top: Val::Px(10.0),
+                    bottom: Val::Px(10.0),
+                    left: Val::Px(0.0),
+                    right: Val::Px(0.0),
+                },
                 flex_direction: FlexDirection::Row,
+                justify_content: JustifyContent::SpaceBetween,
                 ..Default::default()
             },
             color: Color::ORANGE.into(),
@@ -150,6 +164,22 @@ fn spawn_input_binding_row(
         })
         .id();
 
+    let action_text = commands
+        .spawn_bundle(TextBundle {
+            text: Text::with_section(
+                action,
+                text_style.clone(),
+                TextAlignment {
+                    vertical: VerticalAlign::Center,
+                    horizontal: HorizontalAlign::Left,
+                },
+            ),
+            ..Default::default()
+        })
+        .id();
+
+    commands.entity(action_node).add_child(action_text);
+
     let input1_node = commands
         .spawn_bundle(NodeBundle {
             style: Style {
@@ -161,6 +191,22 @@ fn spawn_input_binding_row(
         })
         .id();
 
+    let input1_text = commands
+        .spawn_bundle(TextBundle {
+            text: Text::with_section(
+                input_1,
+                text_style.clone(),
+                TextAlignment {
+                    vertical: VerticalAlign::Center,
+                    horizontal: HorizontalAlign::Center,
+                },
+            ),
+            ..Default::default()
+        })
+        .id();
+
+    commands.entity(input1_node).add_child(input1_text);
+
     let input2_node = commands
         .spawn_bundle(NodeBundle {
             style: Style {
@@ -171,6 +217,22 @@ fn spawn_input_binding_row(
             ..Default::default()
         })
         .id();
+
+    let input2_text = commands
+        .spawn_bundle(TextBundle {
+            text: Text::with_section(
+                input_2,
+                text_style.clone(),
+                TextAlignment {
+                    vertical: VerticalAlign::Center,
+                    horizontal: HorizontalAlign::Center,
+                },
+            ),
+            ..Default::default()
+        })
+        .id();
+
+    commands.entity(input2_node).add_child(input2_text);
 
     commands
         .entity(row_node)
