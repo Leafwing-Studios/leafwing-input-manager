bc4d52e765b9817126dcb620b595a25a981ececf
Alice Cecile <alice.i.cecile@gmail.com>
2022-01-28 19:56:55 -0500

    Clean up path checking

--- macros/src/path.rs
+++ macros/src/path.rs
@@ -1,13 +1,12 @@
-//! This logic is shamelessly stolen from `bevy_macro_utils`
-//! Link: https://github.com/bevyengine/bevy/tree/main/crates/bevy_macro_utils
-//! Copyright 2022, Bevy Contributors
-//! Used under the MIT License
-
-use cargo_manifest::{DepsSet, Manifest};
+use cargo_manifest::Manifest;
 use proc_macro::TokenStream;
 use std::{env, path::PathBuf};
 
-pub struct LeafwingManifest {
+pub(crate) fn get_path() -> syn::Path {
+    LeafwingManifest::default().get_path("leafwing_input_manager")
+}
+
+struct LeafwingManifest {
     manifest: Manifest,
 }
 
@@ -31,31 +30,32 @@ impl LeafwingManifest {
     /// If it cannot be found, assume that the usage is internal
     fn get_path(&self, name: &str) -> syn::Path {
         self.maybe_get_path(name)
-            .unwrap_or_else(|| Self::parse_str("crate"))
+            .unwrap_or_else(|| parse_str("crate"))
     }
 
     /// Get the path of the crate from the dependencies
     fn maybe_get_path(&self, name: &str) -> Option<syn::Path> {
-        let find_in_deps = |deps: &DepsSet| -> Option<syn::Path> {
-            if let Some(dep) = deps.get(name) {
-                Some(Self::parse_str(dep.package().unwrap_or(name)))
-            } else {
-                None
+        // The fallback, in case nothing is found
+        let mut path = None;
+
+        // Check direct dependencies
+        if let Some(ref dependencies) = self.manifest.dependencies {
+            if dependencies.get(name).is_some() {
+                path = Some(parse_str(name));
             }
-        };
+        }
 
-        let deps = self.manifest.dependencies.as_ref();
-        let deps_dev = self.manifest.dev_dependencies.as_ref();
+        // Check dev dependencies
+        if let Some(ref dependencies) = self.manifest.dev_dependencies {
+            if dependencies.get(name).is_some() {
+                path = Some(parse_str(name));
+            }
+        }
 
-        deps.and_then(find_in_deps)
-            .or_else(|| deps_dev.and_then(find_in_deps))
-    }
-
-    fn parse_str<T: syn::parse::Parse>(path: &str) -> T {
-        syn::parse(path.parse::<TokenStream>().unwrap()).unwrap()
+        path
     }
 }
 
-pub(crate) fn get_path() -> syn::Path {
-    LeafwingManifest::default().get_path("leafwing_input_manager")
+fn parse_str<T: syn::parse::Parse>(path: &str) -> T {
+    syn::parse(path.parse::<TokenStream>().unwrap()).unwrap()
 }
