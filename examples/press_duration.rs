8b201fff8f4490e5342213aa962c16e849b94c2f
Alice Cecile <alice.i.cecile@gmail.com>
2022-04-28 15:37:44 -0400

    Improve press_duration example description

--- examples/press_duration.rs
+++ examples/press_duration.rs
@@ -1,5 +1,7 @@
 //! We can check how long each action has been pressed for,
 //! and use that in our gameplay logic!
+//!
+//! Press Left / Right or A / D to move your character to the left and right!
 
 use bevy::prelude::*;
 use leafwing_input_manager::prelude::*;
