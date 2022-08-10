e0384c9fdf708e26475f4a563f657738fbf5cf77
Pulau_Komodo <komodo_helico@example.com>
2022-08-10 18:04:11 +0000

    Add methods to `SingleAxis` and `DualAxis` to set deadzones

--- src/axislike.rs
+++ src/axislike.rs
@@ -101,6 +101,14 @@ impl SingleAxis {
             value: None,
         }
     }
+
+    /// Returns this [`SingleAxis`] with the deadzone set to the specified value
+    #[must_use]
+    pub const fn with_deadzone(mut self, deadzone: f32) -> SingleAxis {
+        self.positive_low = deadzone;
+        self.negative_low = -deadzone;
+        self
+    }
 }
 
 impl PartialEq for SingleAxis {
@@ -207,6 +215,14 @@ impl DualAxis {
             x: SingleAxis::mouse_motion_x(),
             y: SingleAxis::mouse_motion_y(),
         }
+    }
+
+    /// Returns this [`DualAxis`] with the deadzone set to the specified value
+    #[must_use]
+    pub const fn with_deadzone(mut self, deadzone: f32) -> DualAxis {
+        self.x = self.x.with_deadzone(deadzone);
+        self.y = self.y.with_deadzone(deadzone);
+        self
     }
 }
 
