a6660909fde0e6fcb8614130010fb2e8ac45ba42
Tim Jentzsch <tim-jentzsch@gmx.de>
2022-08-13 11:19:50 +0200

    Run 'cargo check' with no features and default features in CI

--- tools/ci/src/main.rs
+++ tools/ci/src/main.rs
@@ -9,11 +9,21 @@ fn main() {
     // See if any code needs to be formatted
     cmd!("cargo fmt --all -- --check")
         .run()
-        .expect("Please run 'cargo fmt --all' to format your code.");
+        .expect("Please run `cargo fmt --all` to format your code.");
 
     // See if clippy has any complaints.
     // - Type complexity must be ignored because we use huge templates for queries
     cmd!("cargo clippy --workspace --all-features -- -D warnings -A clippy::type_complexity")
         .run()
-        .expect("Please fix clippy errors in output above.");
+        .expect("Please fix `cargo clippy` errors with all features enabled.");
+
+    // Check for errors with no features enabled
+    cmd!("cargo check --workspace --no-default-features")
+        .run()
+        .expect("Please fix `cargo check` errors with no features enabled .");
+
+    // Check for errors with default features enabled
+    cmd!("cargo check --workspace")
+        .run()
+        .expect("Please fix `cargo check` errors with default features enabled.");
 }
