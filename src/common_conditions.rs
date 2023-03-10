457997ffee424e94ad3158b0dd8b46b04fc6a278
Hennadii Chernyshchyk <genaloner@gmail.com>
2023-03-10 23:37:13 +0200

    Relax bounds to Clone

--- src/common_conditions.rs
+++ src/common_conditions.rs
@@ -6,11 +6,11 @@ use bevy::prelude::Res;
 /// Stateful run condition that can be toggled via an action press using [`ActionState::just_pressed`].
 pub fn action_toggle_active<T>(default: bool, action: T) -> impl FnMut(Res<ActionState<T>>) -> bool
 where
-    T: Actionlike + Copy,
+    T: Actionlike + Clone,
 {
     let mut active = default;
     move |action_state: Res<ActionState<T>>| {
-        active ^= action_state.just_pressed(action);
+        active ^= action_state.just_pressed(action.clone());
         active
     }
 }
@@ -18,23 +18,23 @@ where
 /// Run condition that is active if [`ActionState::pressed`] is true for the given action.
 pub fn action_pressed<T>(action: T) -> impl FnMut(Res<ActionState<T>>) -> bool
 where
-    T: Actionlike + Copy,
+    T: Actionlike + Clone,
 {
-    move |action_state: Res<ActionState<T>>| action_state.pressed(action)
+    move |action_state: Res<ActionState<T>>| action_state.pressed(action.clone())
 }
 
 /// Run condition that is active if [`ActionState::just_pressed`] is true for the given action.
 pub fn action_just_pressed<T>(action: T) -> impl FnMut(Res<ActionState<T>>) -> bool
 where
-    T: Actionlike + Copy,
+    T: Actionlike + Clone,
 {
-    move |action_state: Res<ActionState<T>>| action_state.just_pressed(action)
+    move |action_state: Res<ActionState<T>>| action_state.just_pressed(action.clone())
 }
 
 /// Run condition that is active if [`ActionState::just_released`] is true for the given action.
 pub fn action_just_released<T>(action: T) -> impl FnMut(Res<ActionState<T>>) -> bool
 where
-    T: Actionlike + Copy,
+    T: Actionlike + Clone,
 {
-    move |action_state: Res<ActionState<T>>| action_state.just_released(action)
+    move |action_state: Res<ActionState<T>>| action_state.just_released(action.clone())
 }
