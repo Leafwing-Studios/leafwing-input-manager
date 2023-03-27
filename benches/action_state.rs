f8305dfb64fe16b70e94f59e9c8ce7506a3d408a
Alice Cecile <alice.i.cecile@gmail.com>
2023-03-27 15:15:28 -0400

    Added simple benchmarks

--- benches/action_state.rs
+++ benches/action_state.rs
@@ -1,9 +1,64 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use bevy::prelude::KeyCode;
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use leafwing_input_manager::{
+    prelude::{ActionState, InputMap},
+    Actionlike,
+};
 
-fn just_pressed() {}
+#[derive(Actionlike, Debug, Clone, Copy, PartialEq, Eq, Hash)]
+enum TestAction {
+    A,
+    B,
+    C,
+    D,
+    E,
+    F,
+    G,
+    H,
+    I,
+    J,
+}
+
+fn simple_input_map() -> InputMap<TestAction> {
+    black_box(InputMap::new([
+        (KeyCode::A, TestAction::A),
+        (KeyCode::B, TestAction::B),
+        (KeyCode::C, TestAction::C),
+        (KeyCode::D, TestAction::D),
+        (KeyCode::E, TestAction::E),
+        (KeyCode::F, TestAction::F),
+        (KeyCode::G, TestAction::G),
+        (KeyCode::H, TestAction::H),
+        (KeyCode::I, TestAction::I),
+        (KeyCode::J, TestAction::J),
+    ]))
+}
+
+fn pressed() -> bool {
+    let action_state = ActionState::<TestAction>::default();
+    action_state.pressed(TestAction::A)
+}
+
+fn just_pressed() -> bool {
+    let action_state = ActionState::<TestAction>::default();
+    action_state.just_pressed(TestAction::A)
+}
+
+fn released() -> bool {
+    let action_state = ActionState::<TestAction>::default();
+    action_state.released(TestAction::A)
+}
+
+fn just_released() -> bool {
+    let action_state = ActionState::<TestAction>::default();
+    action_state.just_released(TestAction::A)
+}
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("fib 20", |b| b.iter(|| just_pressed()));
+    c.bench_function("pressed", |b| b.iter(|| pressed()));
+    c.bench_function("just_pressed", |b| b.iter(|| just_pressed()));
+    c.bench_function("released", |b| b.iter(|| released()));
+    c.bench_function("just_released", |b| b.iter(|| just_released()));
 }
 
 criterion_group!(benches, criterion_benchmark);
--- benches/input_map.rs
+++ benches/input_map.rs
@@ -1,9 +1,62 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use bevy::prelude::{info, KeyCode};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use leafwing_input_manager::{prelude::InputMap, Actionlike};
 
-fn input_map() {}
+#[derive(Actionlike, Debug, Clone, Copy, PartialEq, Eq, Hash)]
+enum TestAction {
+    A,
+    B,
+    C,
+    D,
+    E,
+    F,
+    G,
+    H,
+    I,
+    J,
+}
+
+fn construct_input_map_from_iter() -> InputMap<TestAction> {
+    black_box(InputMap::new([
+        (KeyCode::A, TestAction::A),
+        (KeyCode::B, TestAction::B),
+        (KeyCode::C, TestAction::C),
+        (KeyCode::D, TestAction::D),
+        (KeyCode::E, TestAction::E),
+        (KeyCode::F, TestAction::F),
+        (KeyCode::G, TestAction::G),
+        (KeyCode::H, TestAction::H),
+        (KeyCode::I, TestAction::I),
+        (KeyCode::J, TestAction::J),
+    ]))
+}
+
+fn construct_input_map_from_chained_calls() -> InputMap<TestAction> {
+    black_box(
+        InputMap::default()
+            .insert(KeyCode::A, TestAction::A)
+            .insert(KeyCode::B, TestAction::B)
+            .insert(KeyCode::C, TestAction::C)
+            .insert(KeyCode::D, TestAction::D)
+            .insert(KeyCode::E, TestAction::E)
+            .insert(KeyCode::F, TestAction::F)
+            .insert(KeyCode::G, TestAction::G)
+            .insert(KeyCode::H, TestAction::H)
+            .insert(KeyCode::I, TestAction::I)
+            .insert(KeyCode::J, TestAction::J)
+            .build(),
+    )
+}
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("fib 20", |b| b.iter(|| input_map()));
+    c.bench_function("construct_input_map_from_iter", |b| {
+        b.iter(|| construct_input_map_from_iter())
+    });
+    c.bench_function("construct_input_map_from_chained_calls", |b| {
+        b.iter(|| construct_input_map_from_chained_calls())
+    });
+
+    info!("Finished benchmarking");
 }
 
 criterion_group!(benches, criterion_benchmark);
