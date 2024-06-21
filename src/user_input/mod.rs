e5bf7d2590491a7ce329a0c4c107ce9a264e6b70
Alice Cecile <alice.i.cecile@gmail.com>
2024-06-21 10:49:16 -0400

    Split apart fancy reflection and serde code

--- src/user_input/mod.rs
+++ src/user_input/mod.rs
@@ -77,31 +77,18 @@
 //! [`KeyCode`]: bevy::prelude::KeyCode
 //! [`MouseButton`]: bevy::prelude::MouseButton
 
-use std::any::{Any, TypeId};
-use std::fmt::{Debug, Formatter};
-use std::hash::{Hash, Hasher};
-use std::sync::RwLock;
+use std::fmt::Debug;
 
-use bevy::prelude::App;
-use bevy::reflect::utility::{reflect_hasher, GenericTypePathCell, NonGenericTypeInfoCell};
-use bevy::reflect::{
-    erased_serde, FromReflect, FromType, GetTypeRegistration, Reflect, ReflectDeserialize,
-    ReflectFromPtr, ReflectKind, ReflectMut, ReflectOwned, ReflectRef, ReflectSerialize, TypeInfo,
-    TypePath, TypeRegistration, Typed, ValueInfo,
-};
+use bevy::reflect::{erased_serde, Reflect};
 use dyn_clone::DynClone;
 use dyn_eq::DynEq;
 use dyn_hash::DynHash;
-use once_cell::sync::Lazy;
-use serde::{Deserialize, Deserializer, Serialize, Serializer};
-use serde_flexitos::ser::require_erased_serialize_impl;
-use serde_flexitos::{serialize_trait_object, MapRegistry, Registry};
+use serde::{Deserialize, Serialize};
 
 use crate::axislike::DualAxisData;
 use crate::clashing_inputs::BasicInputs;
 use crate::input_streams::InputStreams;
 use crate::raw_inputs::RawInputs;
-use crate::typetag::RegisterTypeTag;
 
 pub use self::chord::*;
 pub use self::gamepad::*;
@@ -112,6 +99,8 @@ pub mod chord;
 pub mod gamepad;
 pub mod keyboard;
 pub mod mouse;
+mod trait_reflection;
+mod trait_serde;
 
 /// Classifies [`UserInput`]s based on their behavior (buttons, analog axes, etc.).
 #[derive(Debug, Clone, Copy, PartialEq, Reflect, Serialize, Deserialize)]
@@ -239,207 +228,4 @@ pub trait Axislike: UserInput {
 pub trait DualAxislike: UserInput {
     /// Gets the values of this input along the X and Y axes (if applicable).
     fn axis_pair(&self, input_streams: &InputStreams) -> DualAxisData;
-}
-
-dyn_clone::clone_trait_object!(UserInput);
-dyn_eq::eq_trait_object!(UserInput);
-dyn_hash::hash_trait_object!(UserInput);
-
-impl Reflect for Box<dyn UserInput> {
-    fn get_represented_type_info(&self) -> Option<&'static TypeInfo> {
-        Some(Self::type_info())
-    }
-
-    fn into_any(self: Box<Self>) -> Box<dyn Any> {
-        self
-    }
-
-    fn as_any(&self) -> &dyn Any {
-        self
-    }
-
-    fn as_any_mut(&mut self) -> &mut dyn Any {
-        self
-    }
-
-    fn into_reflect(self: Box<Self>) -> Box<dyn Reflect> {
-        self
-    }
-
-    fn as_reflect(&self) -> &dyn Reflect {
-        self
-    }
-
-    fn as_reflect_mut(&mut self) -> &mut dyn Reflect {
-        self
-    }
-
-    fn try_apply(&mut self, value: &dyn Reflect) -> Result<(), bevy::reflect::ApplyError> {
-        let value = value.as_any();
-        if let Some(value) = value.downcast_ref::<Self>() {
-            *self = value.clone();
-            Ok(())
-        } else {
-            Err(bevy::reflect::ApplyError::MismatchedTypes {
-                from_type: self
-                    .reflect_type_ident()
-                    .unwrap_or_default()
-                    .to_string()
-                    .into_boxed_str(),
-                to_type: self
-                    .reflect_type_ident()
-                    .unwrap_or_default()
-                    .to_string()
-                    .into_boxed_str(),
-            })
-        }
-    }
-
-    fn apply(&mut self, value: &dyn Reflect) {
-        Self::try_apply(self, value).unwrap();
-    }
-
-    fn set(&mut self, value: Box<dyn Reflect>) -> Result<(), Box<dyn Reflect>> {
-        *self = value.take()?;
-        Ok(())
-    }
-
-    fn reflect_kind(&self) -> ReflectKind {
-        ReflectKind::Value
-    }
-
-    fn reflect_ref(&self) -> ReflectRef {
-        ReflectRef::Value(self)
-    }
-
-    fn reflect_mut(&mut self) -> ReflectMut {
-        ReflectMut::Value(self)
-    }
-
-    fn reflect_owned(self: Box<Self>) -> ReflectOwned {
-        ReflectOwned::Value(self)
-    }
-
-    fn clone_value(&self) -> Box<dyn Reflect> {
-        Box::new(self.clone())
-    }
-
-    fn reflect_hash(&self) -> Option<u64> {
-        let mut hasher = reflect_hasher();
-        let type_id = TypeId::of::<Self>();
-        Hash::hash(&type_id, &mut hasher);
-        Hash::hash(self, &mut hasher);
-        Some(hasher.finish())
-    }
-
-    fn reflect_partial_eq(&self, value: &dyn Reflect) -> Option<bool> {
-        value
-            .as_any()
-            .downcast_ref::<Self>()
-            .map(|value| self.dyn_eq(value))
-            .or(Some(false))
-    }
-
-    fn debug(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        Debug::fmt(self, f)
-    }
-}
-
-impl Typed for Box<dyn UserInput> {
-    fn type_info() -> &'static TypeInfo {
-        static CELL: NonGenericTypeInfoCell = NonGenericTypeInfoCell::new();
-        CELL.get_or_set(|| TypeInfo::Value(ValueInfo::new::<Self>()))
-    }
-}
-
-impl TypePath for Box<dyn UserInput> {
-    fn type_path() -> &'static str {
-        static CELL: GenericTypePathCell = GenericTypePathCell::new();
-        CELL.get_or_insert::<Self, _>(|| {
-            {
-                format!("std::boxed::Box<dyn {}::UserInput>", module_path!())
-            }
-        })
-    }
-
-    fn short_type_path() -> &'static str {
-        static CELL: GenericTypePathCell = GenericTypePathCell::new();
-        CELL.get_or_insert::<Self, _>(|| "Box<dyn UserInput>".to_string())
-    }
-
-    fn type_ident() -> Option<&'static str> {
-        Some("Box<dyn UserInput>")
-    }
-
-    fn crate_name() -> Option<&'static str> {
-        Some(module_path!().split(':').next().unwrap())
-    }
-
-    fn module_path() -> Option<&'static str> {
-        Some(module_path!())
-    }
-}
-
-impl GetTypeRegistration for Box<dyn UserInput> {
-    fn get_type_registration() -> TypeRegistration {
-        let mut registration = TypeRegistration::of::<Self>();
-        registration.insert::<ReflectDeserialize>(FromType::<Self>::from_type());
-        registration.insert::<ReflectFromPtr>(FromType::<Self>::from_type());
-        registration.insert::<ReflectSerialize>(FromType::<Self>::from_type());
-        registration
-    }
-}
-
-impl FromReflect for Box<dyn UserInput> {
-    fn from_reflect(reflect: &dyn Reflect) -> Option<Self> {
-        Some(reflect.as_any().downcast_ref::<Self>()?.clone())
-    }
-}
-
-impl<'a> Serialize for dyn UserInput + 'a {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        // Check that `UserInput` has `erased_serde::Serialize` as a super trait,
-        // preventing infinite recursion at runtime.
-        const fn __check_erased_serialize_super_trait<T: ?Sized + UserInput>() {
-            require_erased_serialize_impl::<T>();
-        }
-        serialize_trait_object(serializer, self.reflect_short_type_path(), self)
-    }
-}
-
-impl<'de> Deserialize<'de> for Box<dyn UserInput> {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        let registry = unsafe { INPUT_REGISTRY.read().unwrap() };
-        registry.deserialize_trait_object(deserializer)
-    }
-}
-
-/// Registry of deserializers for [`UserInput`]s.
-static mut INPUT_REGISTRY: Lazy<RwLock<MapRegistry<dyn UserInput>>> =
-    Lazy::new(|| RwLock::new(MapRegistry::new("UserInput")));
-
-/// A trait for registering a specific [`UserInput`].
-pub trait RegisterUserInput {
-    /// Registers the specified [`UserInput`].
-    fn register_user_input<'de, T>(&mut self) -> &mut Self
-    where
-        T: RegisterTypeTag<'de, dyn UserInput> + GetTypeRegistration;
-}
-
-impl RegisterUserInput for App {
-    fn register_user_input<'de, T>(&mut self) -> &mut Self
-    where
-        T: RegisterTypeTag<'de, dyn UserInput> + GetTypeRegistration,
-    {
-        let mut registry = unsafe { INPUT_REGISTRY.write().unwrap() };
-        T::register_typetag(&mut registry);
-        self.register_type::<T>();
-        self
-    }
 }
---
+++ src/user_input/trait_serde.rs
@@ -0,0 +1,66 @@
+//! Serialization and deserialization of user input.
+
+use std::sync::RwLock;
+
+use bevy::app::App;
+use bevy::reflect::GetTypeRegistration;
+use once_cell::sync::Lazy;
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use serde_flexitos::ser::require_erased_serialize_impl;
+use serde_flexitos::{serialize_trait_object, MapRegistry, Registry};
+
+use crate::typetag::RegisterTypeTag;
+
+use super::UserInput;
+
+/// Registry of deserializers for [`UserInput`]s.
+static mut INPUT_REGISTRY: Lazy<RwLock<MapRegistry<dyn UserInput>>> =
+    Lazy::new(|| RwLock::new(MapRegistry::new("UserInput")));
+
+/// A trait for registering a specific [`UserInput`].
+pub trait RegisterUserInput {
+    /// Registers the specified [`UserInput`].
+    fn register_user_input<'de, T>(&mut self) -> &mut Self
+    where
+        T: RegisterTypeTag<'de, dyn UserInput> + GetTypeRegistration;
+}
+
+impl RegisterUserInput for App {
+    fn register_user_input<'de, T>(&mut self) -> &mut Self
+    where
+        T: RegisterTypeTag<'de, dyn UserInput> + GetTypeRegistration,
+    {
+        let mut registry = unsafe { INPUT_REGISTRY.write().unwrap() };
+        T::register_typetag(&mut registry);
+        self.register_type::<T>();
+        self
+    }
+}
+
+mod user_input {
+    use super::*;
+
+    impl<'a> Serialize for dyn UserInput + 'a {
+        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer,
+        {
+            // Check that `UserInput` has `erased_serde::Serialize` as a super trait,
+            // preventing infinite recursion at runtime.
+            const fn __check_erased_serialize_super_trait<T: ?Sized + UserInput>() {
+                require_erased_serialize_impl::<T>();
+            }
+            serialize_trait_object(serializer, self.reflect_short_type_path(), self)
+        }
+    }
+
+    impl<'de> Deserialize<'de> for Box<dyn UserInput> {
+        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: Deserializer<'de>,
+        {
+            let registry = unsafe { INPUT_REGISTRY.read().unwrap() };
+            registry.deserialize_trait_object(deserializer)
+        }
+    }
+}
