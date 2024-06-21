e4f07b3575f25d6e7aa2cdaaf373b2cbd2004dd6
Alice Cecile <alice.i.cecile@gmail.com>
2024-06-21 10:56:24 -0400

    Add more reflect and serde impls for split traits

--- src/user_input/trait_serde.rs
+++ src/user_input/trait_serde.rs
@@ -64,3 +64,93 @@ mod user_input {
         }
     }
 }
+
+mod buttonlike {
+    use crate::user_input::Buttonlike;
+
+    use super::*;
+
+    impl<'a> Serialize for dyn Buttonlike + 'a {
+        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer,
+        {
+            // Check that `UserInput` has `erased_serde::Serialize` as a super trait,
+            // preventing infinite recursion at runtime.
+            const fn __check_erased_serialize_super_trait<T: ?Sized + UserInput>() {
+                require_erased_serialize_impl::<T>();
+            }
+            serialize_trait_object(serializer, self.reflect_short_type_path(), self)
+        }
+    }
+
+    impl<'de> Deserialize<'de> for Box<dyn Buttonlike> {
+        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: Deserializer<'de>,
+        {
+            let registry = unsafe { INPUT_REGISTRY.read().unwrap() };
+            registry.deserialize_trait_object(deserializer)
+        }
+    }
+}
+
+mod axislike {
+    use crate::user_input::Axislike;
+
+    use super::*;
+
+    impl<'a> Serialize for dyn Axislike + 'a {
+        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer,
+        {
+            // Check that `UserInput` has `erased_serde::Serialize` as a super trait,
+            // preventing infinite recursion at runtime.
+            const fn __check_erased_serialize_super_trait<T: ?Sized + UserInput>() {
+                require_erased_serialize_impl::<T>();
+            }
+            serialize_trait_object(serializer, self.reflect_short_type_path(), self)
+        }
+    }
+
+    impl<'de> Deserialize<'de> for Box<dyn Axislike> {
+        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: Deserializer<'de>,
+        {
+            let registry = unsafe { INPUT_REGISTRY.read().unwrap() };
+            registry.deserialize_trait_object(deserializer)
+        }
+    }
+}
+
+mod dualaxislike {
+    use crate::user_input::DualAxislike;
+
+    use super::*;
+
+    impl<'a> Serialize for dyn DualAxislike + 'a {
+        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer,
+        {
+            // Check that `UserInput` has `erased_serde::Serialize` as a super trait,
+            // preventing infinite recursion at runtime.
+            const fn __check_erased_serialize_super_trait<T: ?Sized + UserInput>() {
+                require_erased_serialize_impl::<T>();
+            }
+            serialize_trait_object(serializer, self.reflect_short_type_path(), self)
+        }
+    }
+
+    impl<'de> Deserialize<'de> for Box<dyn DualAxislike> {
+        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: Deserializer<'de>,
+        {
+            let registry = unsafe { INPUT_REGISTRY.read().unwrap() };
+            registry.deserialize_trait_object(deserializer)
+        }
+    }
+}
