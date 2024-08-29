88a23ea0500f7552cf5d0d336f27486bf50bf596
Alice Cecile <alice.i.cecile@gmail.com>
2024-08-29 10:19:35 -0400

    More integration testing

--- tests/triggerlike.rs
+++ tests/triggerlike.rs
@@ -6,6 +6,7 @@ use bevy::{
     prelude::*,
 };
 use leafwing_input_manager::prelude::*;
+use updating::CentralInputStore;
 
 #[derive(Actionlike, Debug, PartialEq, Eq, Clone, Copy, Hash, Reflect)]
 enum TestAction {
@@ -35,7 +36,6 @@ fn test_app() -> App {
     let mut gamepad_events: Mut<'_, Events<GamepadEvent>> =
         app.world_mut().resource_mut::<Events<GamepadEvent>>();
     gamepad_events.send(GamepadEvent::Connection(GamepadConnectionEvent {
-        // Must be consistent with mocked events
         gamepad: Gamepad { id: 1 },
         connection: GamepadConnection::Connected(GamepadInfo {
             name: "FirstController".into(),
@@ -48,6 +48,27 @@ fn test_app() -> App {
     app.update();
 
     app
+}
+
+#[test]
+fn set_trigger_updates_central_input_store() {
+    let mut app = test_app();
+
+    let gamepad_trigger = GamepadButtonType::RightTrigger;
+    gamepad_trigger.set_trigger_value(app.world_mut(), 0.7);
+
+    let keycode = KeyCode::Space;
+    keycode.set_trigger_value(app.world_mut(), 1.0);
+
+    app.update();
+
+    let central_input_store = app.world().resource::<CentralInputStore>();
+    assert_eq!(
+        central_input_store.trigger_value(&GamepadButtonType::RightTrigger),
+        0.7
+    );
+    assert_eq!(central_input_store.trigger_value(&KeyCode::Space), 1.0);
+    assert_eq!(central_input_store.pressed(&KeyCode::Space), true);
 }
 
 #[test]
@@ -120,4 +141,54 @@ fn gamepad_trigger() {
     let action_state = app.world().resource::<ActionState<TestAction>>();
     let trigger_value = action_state.trigger_value(&TestAction::Throttle);
     assert_eq!(trigger_value, 0.7);
+}
+
+#[test]
+fn triggerlike_actions_can_be_pressed_and_released_when_pressed() {
+    let mut app = test_app();
+
+    let relevant_button = MouseButton::Left;
+
+    relevant_button.press(app.world_mut());
+    app.update();
+
+    let action_state = app.world().resource::<ActionState<TestAction>>();
+    assert!(action_state.just_pressed(&TestAction::Throttle));
+    assert!(action_state.pressed(&TestAction::Throttle));
+    assert!(!action_state.released(&TestAction::Throttle));
+    assert!(!action_state.just_released(&TestAction::Throttle));
+
+    relevant_button.release(app.world_mut());
+    app.update();
+
+    let action_state = app.world().resource::<ActionState<TestAction>>();
+    assert!(!action_state.just_pressed(&TestAction::Throttle));
+    assert!(!action_state.pressed(&TestAction::Throttle));
+    assert!(action_state.just_released(&TestAction::Throttle));
+    assert!(action_state.released(&TestAction::Throttle));
+}
+
+#[test]
+fn triggerlike_actions_can_be_pressed_and_released_when_trigger_value_set() {
+    let mut app = test_app();
+
+    let gamepad_trigger = GamepadButtonType::RightTrigger;
+    gamepad_trigger.set_trigger_value(app.world_mut(), 1.0);
+    app.update();
+
+    let action_state = app.world().resource::<ActionState<TestAction>>();
+    assert!(action_state.just_pressed(&TestAction::Throttle));
+    assert!(action_state.pressed(&TestAction::Throttle));
+    assert!(!action_state.released(&TestAction::Throttle));
+    assert!(!action_state.just_released(&TestAction::Throttle));
+
+    let gamepad_trigger = GamepadButtonType::RightTrigger;
+    gamepad_trigger.set_trigger_value(app.world_mut(), 0.0);
+    app.update();
+
+    let action_state = app.world().resource::<ActionState<TestAction>>();
+    assert!(!action_state.just_pressed(&TestAction::Throttle));
+    assert!(!action_state.pressed(&TestAction::Throttle));
+    assert!(action_state.just_released(&TestAction::Throttle));
+    assert!(action_state.released(&TestAction::Throttle));
 }
