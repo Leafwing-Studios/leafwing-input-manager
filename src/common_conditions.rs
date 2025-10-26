d31b80744835ee13ba1073062d3a2ec0cb1fc0ff
Joda <JodaInteractive@protonmail.com>
2025-10-25 21:34:53 -0500

    Potential resolution to #627

--- src/common_conditions.rs
+++ src/common_conditions.rs
@@ -1,7 +1,7 @@
 //! Run conditions for actions.
 
 use crate::{prelude::ActionState, Actionlike};
-use bevy::prelude::Res;
+use bevy::{ecs::system::Query, prelude::Res};
 
 /// Stateful run condition that can be toggled via an action press using [`ActionState::just_pressed`].
 pub fn action_toggle_active<A>(default: bool, action: A) -> impl FnMut(Res<ActionState<A>>) -> bool
@@ -15,12 +15,43 @@ where
     }
 }
 
+/// Stateful run condition that can be toggled via an action press using [`ActionState::just_pressed`].
+pub fn action_toggle_active_component<A>(
+    default: bool,
+    action: A,
+) -> impl FnMut(Query<&ActionState<A>>) -> bool
+where
+    A: Actionlike + Clone,
+{
+    let mut active = default;
+    move |action_state: Query<&ActionState<A>>| {
+        if let Ok(action_state) = action_state.single() {
+            active ^= action_state.just_pressed(&action);
+        }
+        active
+    }
+}
+
 /// Run condition that is active if [`ActionState::pressed`] is true for the given action.
 pub fn action_pressed<A>(action: A) -> impl FnMut(Res<ActionState<A>>) -> bool
 where
     A: Actionlike + Clone,
 {
     move |action_state: Res<ActionState<A>>| action_state.pressed(&action)
+}
+
+/// Run condition that is active if [`ActionState::pressed`] is true for the given action.
+pub fn action_pressed_component<A>(action: A) -> impl FnMut(Query<&ActionState<A>>) -> bool
+where
+    A: Actionlike + Clone,
+{
+    move |action_state: Query<&ActionState<A>>| {
+        if let Ok(action_state) = action_state.single() {
+            action_state.pressed(&action)
+        } else {
+            false
+        }
+    }
 }
 
 /// Run condition that is active if [`ActionState::just_pressed`] is true for the given action.
@@ -31,10 +62,38 @@ where
     move |action_state: Res<ActionState<A>>| action_state.just_pressed(&action)
 }
 
+/// Run condition that is active if [`ActionState::just_pressed`] is true for the given action.
+pub fn action_just_pressed_component<A>(action: A) -> impl FnMut(Query<&ActionState<A>>) -> bool
+where
+    A: Actionlike + Clone,
+{
+    move |action_state: Query<&ActionState<A>>| {
+        if let Ok(action_state) = action_state.single() {
+            action_state.just_pressed(&action)
+        } else {
+            false
+        }
+    }
+}
+
 /// Run condition that is active if [`ActionState::just_released`] is true for the given action.
 pub fn action_just_released<A>(action: A) -> impl FnMut(Res<ActionState<A>>) -> bool
 where
     A: Actionlike + Clone,
 {
     move |action_state: Res<ActionState<A>>| action_state.just_released(&action)
+}
+
+/// Run condition that is active if [`ActionState::just_released`] is true for the given action.
+pub fn action_just_released_component<A>(action: A) -> impl FnMut(Query<&ActionState<A>>) -> bool
+where
+    A: Actionlike + Clone,
+{
+    move |action_state: Query<&ActionState<A>>| {
+        if let Ok(action_state) = action_state.single() {
+            action_state.just_released(&action)
+        } else {
+            false
+        }
+    }
 }
